f6f009d75f688c0b5c18700519bd2041ee1c6e32
Bruna Maria <bruna.mariap@outlook.com>
2023-10-04 12:45:20 -0300

    Initial commit

---
+++ js/countdown.js
@@ -0,0 +1,32 @@
+let display = document.getElementById("countdown")
+
+let daysDisplay = document.getElementById("days")
+let hoursDisplay = document.getElementById("hours")
+let minutesDisplay = document.getElementById("minutes")
+let secondsDisplay = document.getElementById("seconds")
+
+let countDownDate = new Date("Dec 10, 2023 10:00:00").getTime();
+
+// Atualiza countDown a cada 1 segundo
+const time = setInterval(() => {
+    let now = new Date().getTime()
+
+    let distance = countDownDate - now
+
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    daysDisplay.innerHTML = days
+    hoursDisplay.innerHTML = hours
+    minutesDisplay.innerHTML = minutes
+    secondsDisplay.innerHTML = seconds
+
+    if (distance < 0) {
+        clearInterval(time);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+
+    console.log(days)
+}, 1000)
